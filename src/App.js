0814d64ad1003200d76aa932ab436721a89c4196
Scott Willrich <swillrich@gmail.com>
2022-11-03 11:48:48 -0500

    pagination implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useState, useEffect } from 'react';
+import Images from './components/Images';
 import "./App.css"
 
 function App() {
-  return (
-    <div className="container mt-5">
-      <h1>Hello from React</h1>
-    </div>
-  );
+
+  const [images, setImages] = useState([])
+  const [loading, setLoading] = useState(false);
+
+
+  useEffect (() => {
+    setLoading(true)
+    fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+      .then(response => response.json()
+      .then(data => setImages(data)));
+    setLoading(false);
+  }, [])
+
+  if (loading) {
+    return (<p>Loading...</p>) 
+  } else {
+    return (
+      <div className="container mt-5">
+        <Images data={ images }/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/Images.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
+
+
+const Images = (props) => {
+    const { data } = props;
+
+    // We start with an empty list of items.
+    const [currentItems, setCurrentItems] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    // Here we use item offsets; we could also use page offsets
+    // following the API or data you're working with.
+    const [itemOffset, setItemOffset] = useState(0);
+
+
+    const itemsPerPage = 6;
+
+    useEffect(() => {
+        const endOffset = itemOffset + itemsPerPage;
+        console.log(`Loading data from ${itemOffset} to ${endOffset}`);
+        setCurrentItems(data.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(data.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage, data]);
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % data.length;
+        console.log(
+        `User requested page number ${event.selected}, which is offset ${newOffset}`
+        );
+        setItemOffset(newOffset);
+    };
+
+    return (
+        <>
+            <div className="images">
+                {currentItems.map(image => {
+                    return (
+                        <div className="image">
+                            <img src={image.url} alt={image.title}/>
+                        </div>
+                    )
+                })}
+            </div>
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={pageCount}
+                previousLabel="< previous"
+                renderOnZeroPageCount={null}
+                containerClassName="pagination"
+                pageLinkClassName="page-num"
+                previousLinkClassName="page-num"
+                nextLinkClassName="page-num"
+                activeLinkClassName="active"
+            />
+        </>
+    );
+}
+
+export default Images;
